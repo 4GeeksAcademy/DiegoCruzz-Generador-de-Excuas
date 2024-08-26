1dcf91dee8a97a3800b2e1ce3ae343643ccb8cd2
Diego Barajas <44250354+DiegoCruzzz@users.noreply.github.com>
2024-08-26 12:38:22 -0600

    Refactor app.js

--- src/app.js
+++ src/app.js
@@ -7,19 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const personas = ["mi perro ", "mi gato ", "mi abuela ", "mi vecino "];
-  const acciones = ["se comio ", "rompio ", "escondio ", "perdio "];
-  const objetos = ["mi tarea ", "mi libro ", "mi telefono ", "mis llaves "];
-  const tiempos = ["anoche", "esta mañana", "ayer", "hace un rato"];
+  const people = ["mi perro ", "mi gato ", "mi abuela ", "mi vecino "];
+  const actions = ["se comio ", "rompio ", "escondio ", "perdio "];
+  const objects = ["mi tarea ", "mi libro ", "mi telefono ", "mis llaves "];
+  const times = ["anoche", "esta mañana", "ayer", "hace un rato"];
 
-  function generarExcusa() {
-    const persona = personas[Math.floor(Math.random() * personas.length)];
-    const accion = acciones[Math.floor(Math.random() * acciones.length)];
-    const objeto = objetos[Math.floor(Math.random() * objetos.length)];
-    const tiempo = tiempos[Math.floor(Math.random() * tiempos.length)];
+  function generateExcuse() {
+    const person = people[Math.floor(Math.random() * personas.length)];
+    const action = actions[Math.floor(Math.random() * acciones.length)];
+    const object = objects[Math.floor(Math.random() * objetos.length)];
+    const time = times[Math.floor(Math.random() * tiempos.length)];
 
-    return persona + accion + objeto + tiempo;
+    return person + action + object + time;
   }
 
-  document.getElementById("excuse").textContent = generarExcusa();
+  document.getElementById("excuse").textContent = generateExcuse();
 };
